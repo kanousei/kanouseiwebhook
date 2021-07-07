12bdb98ef93b20182b613a245ac7fca24826e391
kanousei <86944196+kanousei@users.noreply.github.com>
2021-07-07 16:55:24 +0900

    Add files via upload

---
+++ Protypev1.java
@@ -0,0 +1,18 @@
+package pro;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+public class Protypev1 {
+
+	String channelSecret = '6722502cf6fe4c9c0d19c7ffbe6fc8ff';
+	String httpRequestBody = '...'; // Request body string
+	SecretKeySpec key = new SecretKeySpec(channelSecret.getBytes(), "HmacSHA256");
+	Mac mac = Mac.getInstance("HmacSHA256");
+	mac.init(key);
+	byte[] source = httpRequestBody.getBytes("UTF-8");
+	String signature = Base64.encodeBase64String(mac.doFinal(source));
+	// Compare x-line-signature request header string and the signature
+
+}
+//https://github.com/kanousei/kanouseiwebhook.git
