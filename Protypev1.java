a9b68784cc8ec72c51fdb48f6de36f2ad8cf9a19
kanousei <86944196+kanousei@users.noreply.github.com>
2021-07-08 08:52:48 +0900

    Update Protypev1.java

--- Protypev1.java
+++ Protypev1.java
@@ -2,17 +2,19 @@ package pro;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
 
 public class Protypev1 {
-
-	String channelSecret = '6722502cf6fe4c9c0d19c7ffbe6fc8ff';
-	String httpRequestBody = '...'; // Request body string
-	SecretKeySpec key = new SecretKeySpec(channelSecret.getBytes(), "HmacSHA256");
-	Mac mac = Mac.getInstance("HmacSHA256");
-	mac.init(key);
-	byte[] source = httpRequestBody.getBytes("UTF-8");
-	String signature = Base64.encodeBase64String(mac.doFinal(source));
-	// Compare x-line-signature request header string and the signature
-
+	
+	public static void main(String[] args) {
+		String channelSecret = '6722502cf6fe4c9c0d19c7ffbe6fc8ff';
+		String httpRequestBody = '...'; // Request body string
+		SecretKeySpec key = new SecretKeySpec(channelSecret.getBytes(), "HmacSHA256");
+		Mac mac = Mac.getInstance("HmacSHA256");
+		mac.init(key);
+		byte[] source = httpRequestBody.getBytes("UTF-8");
+		String signature = Base64.encodeBase64String(mac.doFinal(source));
+		// Compare x-line-signature request header string and the signature
+	}	
 }
 //https://github.com/kanousei/kanouseiwebhook.git
